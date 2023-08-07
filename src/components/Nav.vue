3182ffe66a2e31ccf4fcec5d4df19dc6176307f9
OXAYR <uzairmaqsood451@gmail.com>
2023-08-07 17:47:39 +0500

    dropdown applied

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,8 +2,10 @@
   <div>
     <nav class="bg-white py-5 shadow-md fixed top-0 left-0 right-0 font-serif">
       <div class="flex">
-        <p class="px-10 font-semibold">TaskBoard</p>
-        <router-link to="/about" class="color-blue-900"> Home </router-link>
+        <router-link to="/home"
+          ><p class="px-10 font-semibold">TaskBoard</p></router-link
+        >
+        <router-link to="/taskview" class="color-blue-900"> Tasks </router-link>
       </div>
     </nav>
   </div>
--- src/views/TaskView.vue
+++ src/views/TaskView.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <ul class="p-6 divide-y divide-slate-300 text-left">
+    <ul class="p-6 divide-y divide-slate-300 text-left shadow-md">
       <li v-for="(task, index) in tasks" :key="index">
         <p class="font-bold">Task title: {{ task.title }}</p>
         <p class="line-clamp-1"><b>Description:</b> {{ task.desc }}</p>
-        <select v-model="task.status">
-          <option value="todo">To Do</option>
+        <select v-model="task.status" class="border border-black mr-10">
+          <option value="pending">To Do</option>
           <option value="in-progress">In Progress</option>
-          <option value="done">Done</option>
+          <option value="completed">Done</option>
         </select>
         <button @click="deleteTask(index)" class="text-red-500">Delete</button>
       </li>
